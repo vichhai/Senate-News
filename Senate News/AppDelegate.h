85275e786de7c5cb8316b55894998de9f4bd57e3
vichhai <vichhai@UFOs-Network.local>
2015-09-02 13:53:28 +0700

    Initial Commit

---
+++ Senate News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Senate News
+//
+//  Created by vichhai on 9/2/15.
+//  Copyright (c) 2015 GITS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Senate News/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Senate News
+//
+//  Created by vichhai on 9/2/15.
+//  Copyright (c) 2015 GITS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
