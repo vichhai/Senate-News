66099c8903f4f2f710ba09a277ebe8fd05a310cb
bunrong <bunrongleang@gmail.com>
2015-09-03 19:40:31 +0700

    add model

---
+++ Senate News/Post.h
@@ -0,0 +1,13 @@
+//
+//  Post.h
+//  Senate News
+//
+//  Created by Eagles BR on 9/3/15.
+//  Copyright (c) 2015 GITS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Post : NSObject
+
+@end
---
+++ Senate News/Post.m
@@ -0,0 +1,13 @@
+//
+//  Post.m
+//  Senate News
+//
+//  Created by Eagles BR on 9/3/15.
+//  Copyright (c) 2015 GITS. All rights reserved.
+//
+
+#import "Post.h"
+
+@implementation Post
+
+@end
