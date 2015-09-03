ea0c6380020694f66e2ba6f19c6d6ec795d1f102
vichhai <vichhai@UFOs-Network.local>
2015-09-03 08:09:40 +0700

    commit

--- Senate News/HomeViewController.m
+++ Senate News/HomeViewController.m
@@ -9,7 +9,7 @@
 #import "HomeViewController.h"
 #import "SWRevealViewController.h"
 #import "CustomTableViewCell.h"
-@interface HomeViewController () <UITableViewDataSource>
+@interface HomeViewController () <UITableViewDataSource,UITableViewDelegate>
 
 @end
 
@@ -67,8 +67,12 @@
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     CustomTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
     [[cell.shareLabel objectAtIndex:0]setText:@"The race that produced the builders of Angkor developed slowly through the fusion of the Mon-Khmer racial groups of Southern Indochina during the first six centuries of the Christian era. Under Indian influence, two principal centers of civilization developed. The older, in the extreme south of the peninsula was called “Funan” (the name is a Chinese transliteration of the ancient Khmer form of the word “Phnom”, which means “hill”). Funan was a powerful maritime empire that ruled over all the shores of the Gulf of Siam. In the mid-sixth century,"];
-    [[cell.shareLabel objectAtIndex:1] setText:@"Nothing"];
-    [[cell.shareLabel objectAtIndex:2] setText:@"Nothing"];
+    [[cell.shareLabel objectAtIndex:1] setText:@"The race that produced"];
+    [[cell.shareLabel objectAtIndex:2] setText:@"Funan was a powerful"];
     return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [self performSegueWithIdentifier:@"detail" sender:nil];
 }
 @end
