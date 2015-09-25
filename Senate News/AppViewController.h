5ffd69e1241041d97095990c2f62cc08aac05d04
Bunrong Leang <bunrongleang@gmail.com>
2015-09-25 16:32:43 +0700

    about app

---
+++ Senate News/AppViewController.h
@@ -0,0 +1,14 @@
+//
+//  AppViewController.h
+//  Senate News
+//
+//  Created by Jung Taesung on 2015. 9. 25..
+//  Copyright (c) 2015년 GITS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIButton *menu;
+
+@end
